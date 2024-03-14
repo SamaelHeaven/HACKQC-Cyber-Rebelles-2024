a3fafa3ab094e119bd5b92f09460d4a53f35241b
CarlosOscco <danieloscco01230@gmail.com>
2024-03-14 12:50:38 -0400

    add: footer logos and refactor

--- public/templates/legs-template.php
+++ public/templates/legs-template.php
@@ -1,28 +1,29 @@
-<main/>
-<body/>
+</main>
 <footer class="bg-body-tertiary">
-    <div class="container py-5">
-        <div class="row gx-5">
-            <div class="col-lg-3">
-                <a href="index.php"><img src="/public/images/fitQuestLogo.png" alt="logo" width="90" height="80"></a>
-                <div class="mb-3 ms-2">Fit-Quest</div>
+    <div class="container py-3">
+        <div class="d-flex justify-content-between">
+            <div class="pt-2">
+                <a href="index.php"><img src="/public/images/logo_equipe.webp" alt="logo" class="footer-logo"></a>
             </div>
 
-            <div class="col-lg-9 pt-4">
-                <div class="row gx-5">
-                    <div class="col-lg-4 col-md-6 mb-5 mb-lg-0">
-                        <h2 class="mb-4 fs-6">NOUS JOINDRE</h2>
-                        <p><em class="fa-solid fa-envelope me-2"></em>test@exemple.ca</p>
-                    </div>
-                    <div class="col-lg-4 col-md-6 redsocial-logo">
-                        <h2 class="mb-4 fs-6">NOUS SUIVRE</h2>
-                        <a href="index.php"><em class="fa-brands fa-facebook"></em></a>
-                        <a href="index.php"><em class="fa-brands fa-x-twitter"></em></a>
-                        <a href="index.php"><em class="fa-brands fa-linkedin"></em></a>
-                        <a href="index.php"><em class="fa-brands fa-youtube"></em></a>
-                    </div>
+            <div class="pt-3 d-flex info-footer">
+                <div>
+                    <h2 class="mb-4 fs-6">NOUS JOINDRE</h2>
+                    <p><em class="fa-solid fa-envelope me-2"></em>test@exemple.ca</p>
                 </div>
+                <div class="redsocial-logo">
+                    <h2 class="mb-4 fs-6">NOUS SUIVRE</h2>
+                    <a href="index.php"><em class="fa-brands fa-facebook"></em></a>
+                    <a href="index.php"><em class="fa-brands fa-x-twitter"></em></a>
+                    <a href="index.php"><em class="fa-brands fa-linkedin"></em></a>
+                    <a href="index.php"><em class="fa-brands fa-youtube"></em></a>
+                </div>
+            </div>
+
+            <div class="pt-2">
+                <a href="index.php"><img src="/public/images/fitQuestLogo.png" alt="logo" class="footer-logo"></a>
             </div>
         </div>
     </div>
 </footer>
+</body>

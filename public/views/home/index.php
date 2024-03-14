fbcf96b76d8d56a832dde49838fca06eb8762344
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-03-14 13:43:03 -0400

    Feat: Added style for active page in nav

--- public/views/home/index.php
+++ public/views/home/index.php
@@ -1,9 +1,11 @@
 <?php
 
 require_once($_SERVER["DOCUMENT_ROOT"] . "/src/php/models/Template.php");
+require_once $_SERVER["DOCUMENT_ROOT"] . "/src/php/models/CurrentPage.php";
 
 $headTemplate = new Template($_SERVER["DOCUMENT_ROOT"] . "/public/templates/head-template.php");
 $headTemplate->setVariable("title", "FitQuest - Home");
+$headTemplate->setVariable("currentPage", CurrentPage::Home);
 
 $legsTemplate = new Template($_SERVER["DOCUMENT_ROOT"] . "/public/templates/legs-template.php");
 
---
+++ src/php/models/CurrentPage.php
@@ -0,0 +1,6 @@
+<?php
+
+enum CurrentPage {
+    case Home;
+    case About;
+}

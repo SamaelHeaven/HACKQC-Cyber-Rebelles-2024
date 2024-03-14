8f1ae07c0e088f2bef12002800d7518d1189f204
CarlosOscco <danieloscco01230@gmail.com>
2024-03-14 13:21:31 -0400

    refactor: footer v2

--- public/templates/legs-template.php
+++ public/templates/legs-template.php
@@ -1,12 +1,16 @@
 </main>
 <footer class="bg-body-tertiary">
     <div class="container py-3">
-        <div class="d-flex justify-content-between">
-            <div class="pt-2">
-                <a href="index.php"><img src="/public/images/logo_equipe.webp" alt="logo" class="footer-logo"></a>
+        <div class="d-flex">
+            <div class="d-flex flex-column">
+                <div class="pt-2">
+                    <a href="index.php"><img src="/public/images/logo_equipe.webp" alt="logo" class="footer-logo"></a>
+                    <a href="index.php"><img src="/public/images/fitQuestLogo.png" alt="logo" class="footer-logo"></a>
+                </div>
+                <div class="pt-3 mx-auto">© Cyber-Rebelles, 2024</div>
             </div>
 
-            <div class="pt-3 d-flex info-footer">
+            <div class="pt-4 mx-auto d-flex info-footer">
                 <div>
                     <h2 class="mb-4 fs-6">NOUS JOINDRE</h2>
                     <p><em class="fa-solid fa-envelope me-2"></em>test@exemple.ca</p>
@@ -18,10 +22,6 @@
                     <a href="index.php"><em class="fa-brands fa-linkedin"></em></a>
                     <a href="index.php"><em class="fa-brands fa-youtube"></em></a>
                 </div>
-            </div>
-
-            <div class="pt-2">
-                <a href="index.php"><img src="/public/images/fitQuestLogo.png" alt="logo" class="footer-logo"></a>
             </div>
         </div>
     </div>

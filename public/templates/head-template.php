af1283378c0756106236be6051a75a7feeb75a74
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-03-14 10:43:58 -0400

    Feat: Added JS map library

--- public/templates/head-template.php
+++ public/templates/head-template.php
@@ -15,10 +15,16 @@ $title ??= "";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+          crossorigin=""/>
     <link rel="stylesheet" href="/public/stylesheets/styles.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
             crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+            integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+            crossorigin=""></script>
     <script type="module" src="/public/javascript/app.js"></script>
     <title><?= $title ?></title>
 </head>
@@ -26,9 +32,11 @@ $title ??= "";
 <main>
     <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-                FitQuest
-            </a>
+            <header>
+                <a class="navbar-brand" href="#">
+                    <img src="" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                    FitQuest
+                </a>
+            </header>
         </div>
     </nav>

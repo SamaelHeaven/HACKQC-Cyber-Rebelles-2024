e1dadee860df8a08735800d6494ad2865c6d3e43
CarlosOscco <danieloscco01230@gmail.com>
2024-03-14 13:37:23 -0400

    refactor: footer content

--- public/templates/legs-template.php
+++ public/templates/legs-template.php
@@ -1,9 +1,9 @@
 </main>
 <footer class="bg-body-tertiary">
     <div class="container py-3">
-        <div class="d-flex">
+        <div class="d-flex footer-content">
             <div class="d-flex flex-column">
-                <div class="pt-2">
+                <div class="pt-2 footer-logos">
                     <a href="index.php"><img src="/public/images/logo_equipe.webp" alt="logo" class="footer-logo"></a>
                     <a href="index.php"><img src="/public/images/fitQuestLogo.png" alt="logo" class="footer-logo"></a>
                 </div>
@@ -15,7 +15,7 @@
                     <h2 class="mb-4 fs-6">NOUS JOINDRE</h2>
                     <p><em class="fa-solid fa-envelope me-2"></em>test@exemple.ca</p>
                 </div>
-                <div class="redsocial-logo">
+                <div class="social-network-logo">
                     <h2 class="mb-4 fs-6">NOUS SUIVRE</h2>
                     <a href="index.php"><em class="fa-brands fa-facebook"></em></a>
                     <a href="index.php"><em class="fa-brands fa-x-twitter"></em></a>

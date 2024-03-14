aee0b5511a9c8d3ec5e7a21034205707676092ce
CarlosOscco <danieloscco01230@gmail.com>
2024-03-14 14:17:20 -0400

    refactor: redirect links

--- public/templates/legs-template.php
+++ public/templates/legs-template.php
@@ -4,8 +4,8 @@
         <div class="d-flex footer-content">
             <div class="d-flex flex-column">
                 <div class="pt-2 footer-logos mx-auto">
-                    <a href="index.php"><img src="/public/images/fitQuestLogo.png" alt="logo" class="footer-logo"></a>
-                    <a href="index.php"><img src="/public/images/logo_equipe.webp" alt="logo" class="footer-logo"></a>
+                    <a href="/public/views/home/index.php"><img src="/public/images/fitQuestLogo.png" alt="logo" class="footer-logo"></a>
+                    <a href="https://devpost.com/software/hackqc-2024" target="_blank"><img src="/public/images/logo_equipe.webp" alt="logo" class="footer-logo"></a>
                 </div>
                 <div class="pt-3 mx-auto copyright-text">© Cyber-Rebelles, 2024</div>
             </div>
@@ -15,8 +15,8 @@
                     <h2 class="mb-4 fs-6 footer-text">NOUS JOINDRE</h2>
                     <p><em class="fa-solid fa-envelope me-2"></em>test@exemple.ca</p>
                 </div>
-                <div class="social-networks footer-text">
-                    <h2 class="mb-3 fs-6">NOUS SUIVRE</h2>
+                <div class="social-networks">
+                    <h2 class="mb-3 fs-6 footer-text">NOUS SUIVRE</h2>
                     <a href="https://github.com/SamaelHeaven/HackQC-Cyber-Rebelles-2024" target="_blank"><em class="fa-brands fa-github"></em></a>
                     <a href="https://devpost.com/software/hackqc-2024" target="_blank"><img src="/public/images/devposticon.ico" alt="devpost-icon" class="devpost-icon"></a>
                 </div>

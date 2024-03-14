65c6909d03d502f995b8bcfcff331f1d982d08dd
CarlosOscco <danieloscco01230@gmail.com>
2024-03-14 15:55:04 -0400

    add: about text v1

--- public/templates/about-template.php
+++ public/templates/about-template.php
@@ -1,8 +1,18 @@
 <section class="container about-content">
     <h1>À propos de FitQuest</h1>
     <p>
-        Afin d'encourager un mode de vie sain et davantage inciter l’utilisations des installations sportives publiques et gratuites, notre site web répertorie les différents emplacements et infrastrucures liés au sport disponibles dans la région. Selon nous, le sport est très bénéfique pour la santé. Nous avions l’impression que certaines villes ne faisaient pas beaucoup la promotion de leur activités mise en place pour leur citoyens, ce qui est l’une des raisons principales pour laquelle nous avons décidé de nous pencher sur ce sujet. Nous pensons fortement que cela pourrait intéresser une grande partie de la population et ainsi inciter ceux qui veulent faire davantage de sport dans leur municipalité à se lancer et profiter des nombreuse activités qui peuvent être offertes. De plus, notre site permettra alors aux utilisateurs de sélectionner un lieu précis et d’y indiquer les activités qui seront organisé à cette endroit afin d’en faire la promotion et de permettre à l’activités d’être connu par le plus de personnes possible pour que les gens puissent en profiter. Et ce qui est le plus formidable, c’est que n’importe qui aura l’occasion d’organisé une activité sportive Nous croyons alors fortement que l’accès à l’information concernant les activités sportives disponibles
+        Afin d'encourager un mode de vie sain et davantage inciter l’utilisations des installations sportives publiques et gratuites, notre site web répertorie les différents emplacements et infrastrucures liés au sport disponibles dans la région.
     </p>
+    <img src="/public/images/urban-soccer-game-stockcake.jpg" alt="urban-soccer">
+    <p>
+        Selon nous, le sport est très bénéfique pour la santé. Nous avions l’impression que certaines villes ne faisaient pas beaucoup la promotion de leur activités mise en place pour leur citoyens, ce qui est l’une des raisons principales pour laquelle nous avons décidé de nous pencher sur ce sujet.
+    </p>
+
+    <p>Nous pensons fortement que cela pourrait intéresser une grande partie de la population et ainsi inciter ceux qui veulent faire davantage de sport dans leur municipalité à se lancer et profiter des nombreuse activités qui peuvent être offertes.</p>
+
+    <p>De plus, notre site permettra alors aux utilisateurs de sélectionner un lieu précis et d’y indiquer les activités qui seront organisé à cette endroit afin d’en faire la promotion et de permettre à l’activités d’être connu par le plus de personnes possible pour que les gens puissent en profiter.</p>
+
+    <p>Et ce qui est le plus formidable, c’est que n’importe qui aura l’occasion d’organisé une activité sportive Nous croyons alors fortement que l’accès à l’information concernant les activités sportives disponibles.</p>
 </section>
 
 

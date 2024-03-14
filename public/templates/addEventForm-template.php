de1e0fa9ee6596d691b523fcd2d3b51091e456c3
fredd024 <frederick.lefebvre24@gmail.com>
2024-03-14 16:49:24 -0400

    feat: debut du backend du form

--- public/templates/addEventForm-template.php
+++ public/templates/addEventForm-template.php
@@ -1,12 +1,21 @@
+<?php
+$errorMessage ??= "";
+
+?>
+
 <div class="container">
+    <div class="mt-5 mb-4 ">
+        <h1>Ajout d'événement</h1>
+    </div>
+
     <div class="row">
         <div class="col-md-6 mb-3">
-            <label class="form-label" for="company">Organisateur</label>
-            <input class="form-control d-block w-100" type="text" id="company">
+            <label class="form-label" for="organizer">Organisateur</label>
+            <input class="form-control d-block w-100" type="text" id="organizer" name="organizer">
         </div>
         <div class="col-md-6 mb-3">
             <label class="form-label" for="eventName">Nom de l'événement</label>
-            <input class="form-control d-block w-100" type="text" id="eventName">
+            <input class="form-control d-block w-100" type="text" id="eventName" name="eventName">
         </div>
         <div class="mb-3">
             <label class="form-label" for="description">Description</label>
@@ -14,22 +23,32 @@
         </div>
         <div class="col-6 col-md-3 mb-3">
             <label class="form-label" for="dateStart">Date de début</label>
-            <input class="form-control d-block" type="date" id="dateStart">
+            <input class="form-control d-block" type="date" id="dateStart" name="dateStart">
         </div>
         <div class="col-6 col-md-3 mb-3">
             <label class="form-label" for="dateEnd">Date de fin</label>
-            <input class="form-control d-block" type="date" id="dateEnd">
+            <input class="form-control d-block" type="date" id="dateEnd" name="dateEnd">
         </div>
         <div class="col-6 col-md-3 mb-3">
             <label class="form-label" for="timeStart">Heure de départ</label>
-            <input class="form-control d-block" type="time" id="timeStart">
+            <input class="form-control d-block" type="time" id="timeStart" name="timeStart">
         </div>
-        <div class="col-6 col-md-3 mb-5">
+        <div class="col-6 col-md-3 mb-4">
             <label class="form-label" for="timeEnd">Heure de fin</label>
-            <input class="form-control d-block" type="time" id="timeEnd">
+            <input class="form-control d-block" type="time" id="timeEnd" name="timeEnd">
         </div>
         <div class="d-flex justify-content-start mb-3">
             <input class="btn btn-primary" type="submit" value="Ajouter l'événement">
         </div>
+        <?php
+        if ($errorMessage != ""){
+            ?>
+            <div class="d-flex justify-content-center mt-3">
+                <p><?= $errorMessage ?></p>
+            </div>
+
+            <?php
+        }
+        ?>
     </div>
 </div>
--- public/views/addevent/index.php
+++ public/views/addevent/index.php
@@ -11,12 +11,53 @@ $formTemplate = new Template($_SERVER["DOCUMENT_ROOT"] . "/public/templates/addE
 
 $legsTemplate = new Template($_SERVER["DOCUMENT_ROOT"] . "/public/templates/legs-template.php");
 
-?>
-<?= $headTemplate->render() ?>
+$organizer = "";
+$eventName = "";
+$description = "";
+$dateStart = "";
+$dateEnd = "";
+$timeStart = "";
+$timeEnd = "";
+$errorMessage = "";
 
-<div class="d-flex justify-content-center mt-3 ">
-    <h1>Ajout d'événement</h1>
-</div>
+if (isset($_POST['organizer']) &&
+    isset($_POST['eventName']) &&
+    isset($_POST['description']) &&
+    isset($_POST['dateStart']) &&
+    isset($_POST['dateEnd']) &&
+    isset($_POST['timeStart']) &&
+    isset($_POST['timeEnd'])){
+
+    $organizer = $_POST['organizer'];
+    $eventName = $_POST['eventName'];
+    $description = $_POST['description'];
+    $dateStart = $_POST['dateStart'];
+    $dateEnd = $_POST['dateEnd'];
+    $timeStart = $_POST['timeStart'];
+    $timeEnd = $_POST['timeEnd'];
+
+    if (!($organizer == "" ||
+    $eventName == "" ||
+    $description == "" ||
+    $dateStart == "" ||
+    $dateEnd == "" ||
+    $timeStart == "" ||
+    $timeEnd == "")) {
+
+        if ( $timeStart < $timeEnd && $dateStart <= $dateEnd){
+            header('Location: ' . "/public/views/home");
+        } else {
+            $errorMessage = "la date ou l'heure de fin ne peut pas etre inferieure a celle du debut ";
+        }
+
+    } else {
+        $errorMessage = "L'un des champs n'a pas été rempli";
+    }
+}
+
+?>
+
+<?= $headTemplate->render() ?>
 
 <form class="m-4" action="" method="post">
     <?= $formTemplate->render() ?>

37a5e4597cbdf7c947eaba2ee0f28c1adeec9975
CarlosOscco <danieloscco01230@gmail.com>
2024-03-14 17:10:25 -0400

    refactor: about content

--- public/templates/about-template.php
+++ public/templates/about-template.php
@@ -1,19 +1,19 @@
-<section class="container about-content">
+<section class="wrapper-xl about-content">
     <h1>À propos de FitQuest</h1>
     <p>
         Afin d'encourager un mode de vie sain et davantage inciter l’utilisations des installations sportives publiques et gratuites, notre site web répertorie les différents emplacements et infrastrucures liés au sport disponibles dans la région.
     </p>
     <img src="/public/images/urban-soccer-game-stockcake.jpg" alt="urban-soccer">
-    <p>
+    <p class="mb-4">
         Selon nous, le sport est très bénéfique pour la santé. Nous avions l’impression que certaines villes ne faisaient pas beaucoup la promotion de leur activités mise en place pour leur citoyens, ce qui est l’une des raisons principales pour laquelle nous avons décidé de nous pencher sur ce sujet.
     </p>
 
-    <div class="d-lg-flex double-content">
-        <img src="/public/images/joyful-race.jpeg" alt="joyful-race" class="mb-4 mx-auto">
+    <div class="d-lg-flex gap-3">
+        <img src="/public/images/joyful-race.jpeg" alt="joyful-race" class="mx-auto">
         <p class="ms-auto mt-auto mb-auto">Nous pensons fortement que cela pourrait intéresser une grande partie de la population et ainsi inciter ceux qui veulent faire davantage de sport dans leur municipalité à se lancer et profiter des nombreuse activités qui peuvent être offertes.</p>
     </div>
 
-    <div class="d-lg-flex double-content">
+    <div class="d-lg-flex gap-3">
         <p class="me-auto mt-auto mb-auto">De plus, notre site permettra alors aux utilisateurs de sélectionner un lieu précis et d’y indiquer les activités qui seront organisé à cette endroit afin d’en faire la promotion et de permettre à l’activités d’être connu par le plus de personnes possible pour que les gens puissent en profiter.</p>
         <img src="/public/images/locations-sport.png" alt="locations-sport" class="my-2 mx-auto">
     </div>

a528db23c50d66816291bd8091b6039cb21f2c3d
CarlosOscco <danieloscco01230@gmail.com>
2024-03-14 14:08:12 -0400

    refactor: footer font-style and new social networks

--- public/templates/legs-template.php
+++ public/templates/legs-template.php
@@ -7,20 +7,18 @@
                     <a href="index.php"><img src="/public/images/fitQuestLogo.png" alt="logo" class="footer-logo"></a>
                     <a href="index.php"><img src="/public/images/logo_equipe.webp" alt="logo" class="footer-logo"></a>
                 </div>
-                <div class="pt-3 mx-auto">© Cyber-Rebelles, 2024</div>
+                <div class="pt-3 mx-auto copyright-text">© Cyber-Rebelles, 2024</div>
             </div>
 
             <div class="pt-4 mx-auto d-flex info-footer">
                 <div>
-                    <h2 class="mb-4 fs-6">NOUS JOINDRE</h2>
+                    <h2 class="mb-4 fs-6 footer-text">NOUS JOINDRE</h2>
                     <p><em class="fa-solid fa-envelope me-2"></em>test@exemple.ca</p>
                 </div>
-                <div class="social-network-logo">
-                    <h2 class="mb-4 fs-6">NOUS SUIVRE</h2>
-                    <a href="index.php"><em class="fa-brands fa-facebook"></em></a>
-                    <a href="index.php"><em class="fa-brands fa-x-twitter"></em></a>
-                    <a href="index.php"><em class="fa-brands fa-linkedin"></em></a>
-                    <a href="index.php"><em class="fa-brands fa-youtube"></em></a>
+                <div class="social-networks footer-text">
+                    <h2 class="mb-3 fs-6">NOUS SUIVRE</h2>
+                    <a href="https://github.com/SamaelHeaven/HackQC-Cyber-Rebelles-2024" target="_blank"><em class="fa-brands fa-github"></em></a>
+                    <a href="https://devpost.com/software/hackqc-2024" target="_blank"><img src="/public/images/devposticon.ico" alt="devpost-icon" class="devpost-icon"></a>
                 </div>
             </div>
         </div>

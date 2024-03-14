eeea711748a8633e2191fe7976251f3b911ebafc
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-03-14 10:10:07 -0400

    Feat: Added homepage title

--- public/views/home/index.php
+++ public/views/home/index.php
@@ -3,7 +3,7 @@
 require_once($_SERVER["DOCUMENT_ROOT"] . "/src/php/models/Template.php");
 
 $headTemplate = new Template($_SERVER["DOCUMENT_ROOT"] . "/public/templates/head-template.php");
-$headTemplate->setVariable("title", "HackQC");
+$headTemplate->setVariable("title", "FitQuest - Home");
 
 $legsTemplate = new Template($_SERVER["DOCUMENT_ROOT"] . "/public/templates/legs-template.php");
 

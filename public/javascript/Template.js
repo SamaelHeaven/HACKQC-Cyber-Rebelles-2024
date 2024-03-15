16a4ad3ac31a2ef522a94e6f7cfc9062e0b7a799
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-03-15 00:05:03 -0400

    Fix: Logo was over some elements

--- public/javascript/Template.js
+++ public/javascript/Template.js
@@ -4,13 +4,13 @@ export class Template {
         <div class="my-4">
             <ul class="nav nav-pills">
                 <li class="nav-item">
-                    <button data-nav-link="info" class="nav-link active">Info</button>
+                    <button data-nav-link="info" class="nav-link active onTop">Info</button>
                 </li>
                 <li class="nav-item">
-                    <button data-nav-link="events" class="nav-link">Événements</button>
+                    <button data-nav-link="events" class="nav-link onTop">Événements</button>
                 </li>
             </ul>
-            <hr>
+            <hr class="onTop">
             <div data-map-panel-section="${sportTerrain.id}">
                 ${Template.getInfoPanel(sportTerrain)}
             </div>
@@ -19,7 +19,7 @@ export class Template {
     }
     static getInfoPanel(sportTerrain) {
         return `
-            <table style="position:relative; z-index: 1" class="table table-responsive table-striped">
+            <table class="table table-responsive table-striped onTop">
                 <tbody>
                     <tr>
                         <th scope="row">Type de terrain</th>
@@ -55,7 +55,7 @@ export class Template {
     }
     static getEventsPanel(sportTerrain, events) {
         let result = "<div class='mb-3'>";
-        result += `<a style="position:relative; z-index: 1" class="btn btn-secondary w-100 fs-4 fw-bold opac" href="/public/views/addevent/?terrainId=${sportTerrain.id}"><i class="fa-solid fa-plus"></i> Ajouter un événement</a>`;
+        result += `<a class="btn btn-secondary onTop w-100 fs-4 fw-bold opac" href="/public/views/addevent/?terrainId=${sportTerrain.id}"><i class="fa-solid fa-plus"></i> Ajouter un événement</a>`;
         for (let event of events) {
             result += `
                 <a style="background: #ffffff; position: relative; z-index: 1" class="border rounded p-3 text-decoration-none text-black border border-2 mt-3 w-100 fw-bold d-flex justify-content-between align-items-center gap-3 flex-wrap" href="/public/views/event/?id=${event.id}">
@@ -66,7 +66,7 @@ export class Template {
         }
         if (events.length === 0) {
             result += `
-                <div class="alert alert-info mt-3" role="alert">
+                <div class="alert alert-info mt-3 onTop" role="alert">
                     Aucun événements n'est associé à ce terrain
                 </div>
             `;
--- src/typescript/Template.ts
+++ src/typescript/Template.ts
@@ -7,13 +7,13 @@ export class Template {
         <div class="my-4">
             <ul class="nav nav-pills">
                 <li class="nav-item">
-                    <button data-nav-link="info" class="nav-link active">Info</button>
+                    <button data-nav-link="info" class="nav-link active onTop">Info</button>
                 </li>
                 <li class="nav-item">
-                    <button data-nav-link="events" class="nav-link">Événements</button>
+                    <button data-nav-link="events" class="nav-link onTop">Événements</button>
                 </li>
             </ul>
-            <hr>
+            <hr class="onTop">
             <div data-map-panel-section="${sportTerrain.id}">
                 ${Template.getInfoPanel(sportTerrain)}
             </div>
@@ -23,7 +23,7 @@ export class Template {
 
     public static getInfoPanel(sportTerrain: SportTerrain): string {
         return `
-            <table style="position:relative; z-index: 1" class="table table-responsive table-striped">
+            <table class="table table-responsive table-striped onTop">
                 <tbody>
                     <tr>
                         <th scope="row">Type de terrain</th>
@@ -60,7 +60,7 @@ export class Template {
 
     public static getEventsPanel(sportTerrain: SportTerrain, events: Event[]): string {
         let result = "<div class='mb-3'>";
-        result += `<a style="position:relative; z-index: 1" class="btn btn-secondary w-100 fs-4 fw-bold opac" href="/public/views/addevent/?terrainId=${sportTerrain.id}"><i class="fa-solid fa-plus"></i> Ajouter un événement</a>`;
+        result += `<a class="btn btn-secondary onTop w-100 fs-4 fw-bold opac" href="/public/views/addevent/?terrainId=${sportTerrain.id}"><i class="fa-solid fa-plus"></i> Ajouter un événement</a>`;
         for (let event of events) {
             result += `
                 <a style="background: #ffffff; position: relative; z-index: 1" class="border rounded p-3 text-decoration-none text-black border border-2 mt-3 w-100 fw-bold d-flex justify-content-between align-items-center gap-3 flex-wrap" href="/public/views/event/?id=${event.id}">
@@ -71,7 +71,7 @@ export class Template {
         }
         if (events.length === 0) {
             result += `
-                <div class="alert alert-info mt-3" role="alert">
+                <div class="alert alert-info mt-3 onTop" role="alert">
                     Aucun événements n'est associé à ce terrain
                 </div>
             `

587699e78b4194fdfea93022396ad3c85310b954
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-03-15 00:24:37 -0400

    Refactor: Minor changes in footer

--- public/templates/legs-template.php
+++ public/templates/legs-template.php
@@ -15,15 +15,15 @@ $insideMain ??= true;
         <div class="d-flex footer-content">
             <div class="d-flex flex-column">
                 <div class="pt-2 footer-logos mx-auto">
-                    <a href="/public/views/home/index.php"><img src="/public/images/fit-quest-logo.png" alt="logo"
+                    <a href="/public/views/home/index.php"><img src="/public/images/fit-quest-logo.png" alt="FitQuest"
                                                                 class="footer-logo"></a>
                     <a href="https://devpost.com/software/hackqc-2024" target="_blank"><img
-                                src="/public/images/logo_equipe.webp" alt="logo" class="footer-logo"></a>
+                                src="/public/images/cyber-rebelles-logo.png" alt="Cyber-Rebelles" class="footer-logo rounded-circle border border-2 shadow"></a>
                 </div>
-                <div class="pt-3 mx-auto copyright-text">© Cyber-Rebelles, 2024</div>
+                <div class="pt-3 mx-auto copyright-text">© Cyber-Rebelles 2024</div>
             </div>
 
-            <div class="pt-4 mx-auto d-flex info-footer">
+            <div class="pt-4 mx-auto d-flex flex-wrap info-footer">
                 <div>
                     <h2 class="mb-4 fs-6 footer-text fw-light">NOUS JOINDRE</h2>
                     <p><em class="fa-solid fa-envelope me-2"></em>cyberebellesinfo@gmail.com</p>

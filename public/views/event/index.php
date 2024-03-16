ca1152950759cc6c4207a95df7139fa453e42a41
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-03-16 01:23:45 -0400

    Refactor: Extracted event details into a template

--- public/views/event/index.php
+++ public/views/event/index.php
@@ -9,7 +9,6 @@ $headTemplate->setVariable("title", "FitQuest - Événement");
 
 $legsTemplate = new Template($_SERVER["DOCUMENT_ROOT"] . "/src/php/templates/legs-template.php");
 
-
 if (isset($_POST['deleteEvent'])) {
     $eventToDeleteId = $_POST['deleteEvent'];
 
@@ -44,65 +43,10 @@ if ($event === null || sizeof($event) === 0) {
 
 $event = $event[0];
 
-$sportTerrain = DatabaseService::query("SELECT * FROM sport_terrain WHERE id = '" . $event['sport_terrain_id'] . "'")[0];
-
-if ($sportTerrain === null) {
-    header('location: /public/views/home/');
-}
+$eventDetailsTemplate = new Template($_SERVER["DOCUMENT_ROOT"] . "/src/php/templates/event-details-template.php");
+$eventDetailsTemplate->setVariable("event", $event);
 
 ?>
 <?= $headTemplate->render() ?>
-    <div class="mx-2">
-        <div class="wrapper-md my-4 border border-2 bg-light rounded p-3">
-            <div class="d-flex justify-content-between align-items-center gap-2 flex-wrap">
-                <button class="btn btn-secondary" onclick="history.back()">
-                    <i class="fa-solid fa-arrow-left"></i>
-                </button>
-                <h2>
-                    <?= $event['event_name'] ?>
-                </h2>
-                <form action="index.php" method="post">
-                    <button type="submit" name="deleteEvent" value="<?= $event['id'] ?>" class="btn btn-danger">
-                        <i class="fa-solid fa-trash"></i>
-                    </button>
-                </form>
-            </div>
-            <hr>
-            <div class="pb-2">
-                <span class="fw-bold">Organisateur:</span> <?= $event['organizer'] ?>
-            </div>
-            <div class="pb-2">
-                <span class="fw-bold">Date:</span> Du <?= $event['start_date'] ?>
-                : <?= substr($event['start_time'], 0, 5) ?>
-                à <?= $event['start_date'] ===  $event['end_date'] ? "" : $event['end_date'] . " : " ?><?= substr($event['end_time'], 0, 5) ?>
-            </div>
-            <p>
-                <span class="fw-bold">Description:</span> <?= $event['description'] ?>
-            </p>
-            <table class="table table-responsive table-striped">
-                <tbody>
-                <tr>
-                    <th scope="row">Type de terrain</th>
-                    <td><?= $sportTerrain["terrain"] . " - " . $sportTerrain["type"] ?></td>
-                </tr>
-                <tr>
-                    <th scope="row">Revêtement du sol</th>
-                    <td><?= $sportTerrain["flooring"] ?></td>
-                </tr>
-                <tr>
-                    <th scope="row">Municipalité</th>
-                    <td><?= $sportTerrain["city"] ?></td>
-                </tr>
-                <tr>
-                    <th scope="row">Longitude</th>
-                    <td><?= $sportTerrain["longitude"] ?></td>
-                </tr>
-                <tr>
-                    <th scope="row">Latitude</th>
-                    <td><?= $sportTerrain["latitude"] ?></td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+<?= $eventDetailsTemplate->render() ?>
 <?= $legsTemplate->render() ?>
---
+++ src/php/templates/event-details-template.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * array $event: The event of the page
+ */
+
+require_once($_SERVER["DOCUMENT_ROOT"] . "/src/php/services/DatabaseService.php");
+
+if (!isset($event)) {
+    exit;
+}
+
+$sportTerrain = DatabaseService::query("SELECT * FROM sport_terrain WHERE id = '" . $event['sport_terrain_id'] . "'");
+
+if ($sportTerrain === null || sizeof($sportTerrain) === 0) {
+    header('location: /public/views/home/');
+}
+
+$sportTerrain = $sportTerrain[0];
+
+?>
+<div class="mx-2">
+    <div class="wrapper-md my-4 border border-2 bg-light rounded p-3">
+        <div class="d-flex justify-content-between align-items-center gap-2 flex-wrap">
+            <button class="btn btn-secondary" onclick="history.back()">
+                <i class="fa-solid fa-arrow-left"></i>
+            </button>
+            <h2>
+                <?= $event['event_name'] ?>
+            </h2>
+            <form action="/public/views/event/" method="post">
+                <button type="submit" name="deleteEvent" value="<?= $event['id'] ?>" class="btn btn-danger">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </form>
+        </div>
+        <hr>
+        <div class="pb-2">
+            <span class="fw-bold">Organisateur:</span> <?= $event['organizer'] ?>
+        </div>
+        <div class="pb-2">
+            <span class="fw-bold">Date:</span> Du <?= $event['start_date'] ?>
+            : <?= substr($event['start_time'], 0, 5) ?>
+            à <?= $event['start_date'] === $event['end_date'] ? "" : $event['end_date'] . " : " ?><?= substr($event['end_time'], 0, 5) ?>
+        </div>
+        <p>
+            <span class="fw-bold">Description:</span> <?= $event['description'] ?>
+        </p>
+        <table class="table table-responsive table-striped">
+            <tbody>
+            <tr>
+                <th scope="row">Type de terrain</th>
+                <td><?= $sportTerrain["terrain"] . " - " . $sportTerrain["type"] ?></td>
+            </tr>
+            <tr>
+                <th scope="row">Revêtement du sol</th>
+                <td><?= $sportTerrain["flooring"] ?></td>
+            </tr>
+            <tr>
+                <th scope="row">Municipalité</th>
+                <td><?= $sportTerrain["city"] ?></td>
+            </tr>
+            <tr>
+                <th scope="row">Longitude</th>
+                <td><?= $sportTerrain["longitude"] ?></td>
+            </tr>
+            <tr>
+                <th scope="row">Latitude</th>
+                <td><?= $sportTerrain["latitude"] ?></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
